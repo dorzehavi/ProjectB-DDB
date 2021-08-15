f7385cd98c9c13f4f7126c1fa29883e40c2f87f8
dor0zehavi <dor0zehavi@techniondbcourse01.database.windows.net>
2021-08-15 11:04:24 +0300

    creating tables in sqlServer- fixed

--- extract_transform_load.py
+++ extract_transform_load.py
@@ -1,6 +1,6 @@
-import pyodbc
 import pandas as pd
 import findspark
+
 findspark.init()
 from pyspark.sql import SparkSession
 import pyspark.sql.functions as F
@@ -8,10 +8,12 @@ from pyspark import SparkFiles
 import os
 from pyspark.sql.types import *
 
+
 def init_spark(app_name: str):
-  spark = SparkSession.builder.appName(app_name).getOrCreate()
-  sc = spark.sparkContext
-  return spark, sc
+    spark = SparkSession.builder.appName(app_name).getOrCreate()
+    sc = spark.sparkContext
+    return spark, sc
+
 
 os.environ['PYSPARK_SUBMIT_ARGS'] = "--packages=org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.4 pyspark-shell"
 
@@ -26,6 +28,7 @@ os.environ[
 
 kafka_server = 'dds2020s-kafka.eastus.cloudapp.azure.com:9092'
 
+
 def get_topics():
     """
     :return: string\list of all the countries code
@@ -38,7 +41,7 @@ def get_topics():
             countries.add(station[0:2])
     for country in list(countries):
         tmp = tmp + str(country) + ","
-    return tmp[:len(tmp)-1]
+    return tmp[:len(tmp) - 1]
 
 
 def create_tables(df):
@@ -47,19 +50,6 @@ def create_tables(df):
     :param df:
     :return:
     """
-    conn = pyodbc.connect('DRIVER={SQL Server};'
-                           'SERVER=technionddscourse.database.windows.net;'
-                           'DATABASE=dor0zehavi;UID=dor0zehavi;PWD=Qwerty12!')
-    cursor = conn.cursor()
-    cursor.execute('''
-               CREATE TABLE Stations(
-        StationId varchar(255) PRIMARY KEY,
-        latitude varchar (255),
-        longitude varchar (255),
-        elevation varchar (255),
-        );''')
-    cursor.commit()
-    cursor.close()
 
     stations_path = 'http://noaa-ghcn-pds.s3.amazonaws.com/ghcnd-stations.txt'
     inventory_path = 'http://noaa-ghcn-pds.s3.amazonaws.com/ghcnd-inventory.txt'
@@ -67,10 +57,17 @@ def create_tables(df):
     sc.addFile(inventory_path)
 
     stations = spark.read.text(SparkFiles.get('ghcnd-stations.txt'))
+    """station_schema = StructType([StructField('StationId', StringType(), False),
+                                 StructField('latitude', FloatType(), False),
+                                 StructField('longitude', FloatType(), False),
+                                 StructField('elevation', FloatType(), False)])"""
     stations = stations.select(stations.value.substr(0, 11).alias('StationId'),
                                stations.value.substr(13, 8).alias('latitude'),
                                stations.value.substr(22, 8).alias('longitude'),
                                stations.value.substr(32, 6).alias('elevation')).dropDuplicates(['StationId'])
+    for col in ['latitude', 'longitude', 'elevation']:
+        stations = stations.withColumn(col, stations[col].cast("float"))
+
     server_name = "jdbc:sqlserver://technionddscourse.database.windows.net:1433"
     database_name = "dor0zehavi"
     url = server_name + ";" + "databaseName=" + database_name + ";"
@@ -78,7 +75,7 @@ def create_tables(df):
     inventory_table = "Inventory"
     username = "dor0zehavi"
     password = "Qwerty12!"
-
+    print()
     stations.write \
         .format("jdbc") \
         .mode("overwrite") \
@@ -99,9 +96,7 @@ def create_tables(df):
         .save()
 
 
-
 if __name__ == '__main__':
-    print(get_topics())
     print()
     # Define the schema of the data:
     noaa_schema = StructType([StructField('StationId', StringType(), False),
@@ -118,22 +113,20 @@ if __name__ == '__main__':
         .option("subscribe", "US") \
         .option("startingOffsets", "earliest") \
         .load()
-    #TODO: notice i have subscribed only to USA, its just for quick working- need to sub. to more topics (get_topics)
+    # TODO: notice i have subscribed only to USA, its just for quick working- need to sub. to more topics (get_topics)
     kafka_value_df = kafka_raw_df.selectExpr("CAST(value AS STRING)")
     json_df = kafka_value_df.select(F.from_json(F.col("value"), schema=noaa_schema).alias('json'))
     # Flatten the nested object:
     kafka_df = json_df.select("json.*")
-    #TODO: maybe to choose different variables to filter by?
+    # TODO: maybe to choose different variables to filter by?
     vars_df = kafka_df.filter((kafka_df['Variable'] == 'PRCP') |
-                         (kafka_df['Variable'] == 'SNOW') | (kafka_df['Variable'] == 'SNWD') |
-                         (kafka_df['Variable'] == 'TMAX') | (kafka_df['Variable'] == 'TMIN') |
-                         (kafka_df['Variable'] == 'ACMC') | (kafka_df['Variable'] == 'AWND') |
-                         (kafka_df['Variable'] == 'DAPR') | (kafka_df['Variable'] == 'MDSF') |
-                         (kafka_df['Variable'] == 'DATN') | (kafka_df['Variable'] == 'DATX') |
-                         (kafka_df['Variable'] == 'DWPR') | (kafka_df['Variable'] == 'MDPR') |
-                         (kafka_df['Variable'] == 'MDSF') | (kafka_df['Variable'] == 'EVAP'))
+                              (kafka_df['Variable'] == 'SNOW') | (kafka_df['Variable'] == 'SNWD') |
+                              (kafka_df['Variable'] == 'TMAX') | (kafka_df['Variable'] == 'TMIN') |
+                              (kafka_df['Variable'] == 'ACMC') | (kafka_df['Variable'] == 'AWND') |
+                              (kafka_df['Variable'] == 'DAPR') | (kafka_df['Variable'] == 'MDSF') |
+                              (kafka_df['Variable'] == 'DATN') | (kafka_df['Variable'] == 'DATX') |
+                              (kafka_df['Variable'] == 'DWPR') | (kafka_df['Variable'] == 'MDPR') |
+                              (kafka_df['Variable'] == 'MDSF') | (kafka_df['Variable'] == 'EVAP'))
     vars_flags_df = vars_df.filter("Q_flag is null").filter('M_Flag is null').filter('S_Flag is not null')
-    #TODO: upload vars_flags_df to Azure
-    #vars_flags_df.show()
     create_tables(vars_flags_df)
     print()
