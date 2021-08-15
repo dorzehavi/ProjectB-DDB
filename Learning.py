0b69af82c17c6b608786316cf3fd7c5e69892787
Gregory Skliar <gregory.sk@campus.technion.ac.il>
2021-08-15 10:42:27 +0300

    Updated dist_metric

--- Learning.py
+++ Learning.py
@@ -9,10 +9,12 @@ from pyspark import SparkFiles
 import os
 from pyspark.sql.types import *
 
+
 def init_spark(app_name: str):
   spark = SparkSession.builder.appName(app_name).getOrCreate()
   sc = spark.sparkContext
   return spark, sc
+
 
 os.environ['PYSPARK_SUBMIT_ARGS'] = "--packages=org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.4 pyspark-shell"
 
@@ -24,6 +26,7 @@ os.environ['PYSPARK_SUBMIT_ARGS'] = "--packages=org.apache.spark:spark-sql-kafka
 os.environ[
     'PYSPARK_SUBMIT_ARGS'] = "--packages=org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.1," \
                              "com.microsoft.azure:spark-mssql-connector_2.12:1.1.0 pyspark-shell"
+
 
 def load(table_name):
     server_name = "jdbc:sqlserver://technionddscourse.database.windows.net:1433"
@@ -41,19 +44,24 @@ def load(table_name):
 
 
 def dist_metric(station1, station2):
+    """
+    :param station1: station with index access
+    :param station2: station with column access
+    """
     sum = 0
-    for column in ["latitude", "longitude", "elevation"]:
-        diff = station1[column] - station2[column]
+    for i, column in enumerate(["latitude", "longitude", "elevation"]):
+        diff = station1[i] - station2[column]
         sum += diff**2
     return np.sqrt(sum)
 
+
 def create_dist_func(station):
-    return lambda row: dist_metric(station, row)
+    return lambda row: dist_metric(row, station)
 
 
 class Predictor:
-    def __init__(self, K, variable):
-        self.k = K
+    def __init__(self, k, variable):
+        self.k = k
         self.variable = variable
         self.data = load("DATA")
         self.stations = load("Stations")
